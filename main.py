f71abdd3ed388fd52b8a1c07621e14e0069d1c9a
abdimamme8-ux <abdimamme8@gmail.com>
2026-07-19 05:22:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from config import BOT_TOKEN
 from handlers.start import router as start_router
 from handlers.admin import router as admin_router
 
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
@@ -13,6 +14,7 @@ dp = Dispatcher()
 async def main():
     dp.include_router(start_router)
     dp.include_router(admin_router)
+
     print("AhmedPromotionBot hojii jalqabe...")
 
     await dp.start_polling(bot)
